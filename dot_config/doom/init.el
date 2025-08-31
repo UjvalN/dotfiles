8926dcb8221f80e75cf7c1f9310b0cd14b108e8b
Ujn <ujnallur@gmail.com>
2025-08-31 18:25:59 -0500

    updates

--- dot_config/doom/init.el
+++ dot_config/doom/init.el
@@ -155,8 +155,7 @@
        (org
         +roam2
         +dragndrop
-        +attach
-        +pretty)              ; organize your plain life in plain text
+        +attach)              ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;graphviz          ; diagrams for confusing yourself even more
